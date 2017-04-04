4769437a836d632762f0105de7106bfe8ac77168
Austin Lee <ucaustinlee@g.ucla.edu>
2017-04-03 23:01:49 -0700

    experiences page template

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
 $(function() {
   $('#exp').hover(function() {
-    $('#overlay').css('background-color', 'rgba(255, 255, 102, 0.5)');
+    $('#overlay').css('background-color', 'rgba(255, 215, 0, 0.5)');
   }, function() {
     // on mouseout, reset the background colour
     $('#overlay').css('background-color', '');
   });
     $('#food').hover(function() {
-    $('#overlay').css('background-color', 'rgba(255, 69, 0, 0.5)');
+    $('#overlay').css('background-color', 'rgba(255, 140, 0, 0.5)');
   }, function() {
     // on mouseout, reset the background colour
     $('#overlay').css('background-color', '');
   });
     $('#perform').hover(function() {
-    $('#overlay').css('background-color', 'rgba(205, 92, 92, 0.5)');
+    $('#overlay').css('background-color', 'rgba(139, 0, 0, 0.5)');
   }, function() {
     // on mouseout, reset the background colour
     $('#overlay').css('background-color', '');
