2ef35eb8cb8799efdf909dddb60bc1b13d96fd48
Austin Lee <ucaustinlee@g.ucla.edu>
2017-04-20 21:15:04 -0700

    overlay text

---
+++ js/exp.js
@@ -0,0 +1,48 @@
+var photosheetkey = "1wxIlU9pjKDcAfZrX4XmzAfCBA1cpWP1TrmoUGpvWa-c";
+var recapsheetkey = "1ffCXCveuwLrG7jL20bTsATxGIxSWDvjTloNQAn_3H-M";
+var photocard_template = $("#card-template").html();
+var photocard_compiled = Handlebars.compile(photocard_template);
+
+Handlebars.registerHelper("formatcaptiontext", function(t) {
+	t = t.trim();
+	var re = new RegExp('[\r\n]+', 'g');
+    return (t.length>0?'<p class="cardcaption">'+t.replace(re,'</p><p class="cardcaption">')+'</p>':null);
+});
+
+init();
+
+function init() {
+	var photosheeturl = "https://spreadsheets.google.com/feeds/list/" + photosheetkey + "/od6/public/values?alt=json"; 
+	$.getJSON(photosheeturl, function(data) {
+		data = clean_google_sheet_json(data);
+		data = data.reverse();
+		$(".content-main").append(photocard_compiled({carddata: data}));
+		
+	});	
+function clean_google_sheet_json(data){
+	var formatted_json = [];
+	var elem = {};
+	var real_keyname = '';
+	$.each(data.feed.entry, function(i, entry) {
+		elem = {};
+		$.each(entry, function(key, value){
+			// fields that were in the spreadsheet start with gsx$
+			if (key.indexOf("gsx$") == 0) 
+			{
+				// get everything after gsx$
+				real_keyname = key.substring(4); 
+				elem[real_keyname] = value['$t'];
+			}
+		});
+		formatted_json.push(elem);
+	});
+	return formatted_json;
+};
+}
+
+function format_body_text(t) {
+	t = t.trim();
+	var re = new RegExp('[\r\n]+', 'g');
+    var str = t.length>0?'<p>'+t.replace(re,'</p>\n<p>')+'</p>':null;
+    return (str.split('\n'));
+}
